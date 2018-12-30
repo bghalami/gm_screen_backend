6e7be1c8628238bd9ca298e1d6957b225a600307
bghalami <benjaminghalami@gmail.com>
2018-12-30 15:36:33 -0700

    Created campaign model

---
+++ src/main/java/com/model/Campaign.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.model;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import javax.persistence.*;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+/**
+ *
+ * @author bghalami
+ */
+
+@Entity
+@Table(name = "campaigns")
+public class Campaign extends AuditModel {
+    @Id
+    @GeneratedValue(generator = "campaign_generator")
+    @SequenceGenerator(
+            name = "campaign_generator",
+            sequenceName = "campaign_sequence",
+            initialValue = 1
+    )
+    private Long id;
+    
+    @NotBlank
+    @Size(min = 3, max = 100)
+    private String title;
+    
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "game_master_id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JsonIgnore
+    private GameMaster gameMaster;
+}
