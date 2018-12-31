b29bdcc2b321dbd5f1adb3a77f06d124cadb1ed1
bghalami <benjaminghalami@gmail.com>
2018-12-30 18:27:49 -0700

    Added encouter model and encouter repository

---
+++ src/main/java/com/repository/EncounterRepository.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.repository;
+
+import com.model.Encounter;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+/**
+ *
+ * @author bghalami
+ */
+@Repository
+public interface EncounterRepository extends JpaRepository<Encounter, Long> {
+    List<Encounter> findByCampaignrId(Long campaignId);
+}
