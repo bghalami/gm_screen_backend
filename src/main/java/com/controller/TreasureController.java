1b64b690f45b0491fb0e3462e6cee1db35195236
bghalami <benjaminghalami@gmail.com>
2019-01-02 16:03:28 -0700

    Added PUT and DELETE to treasure table

--- src/main/java/com/controller/TreasureController.java
+++ src/main/java/com/controller/TreasureController.java
@@ -26,7 +26,7 @@ public class TreasureController {
     @Autowired
     private CharacterRepository characterRepository;
     
-     @PostMapping("/api/v1/characters/{characterId}/treasures")
+    @PostMapping("/api/v1/characters/{characterId}/treasures")
     public Treasure addTreasure(@PathVariable Long characterId,
                                 @Valid @RequestBody Treasure treasure
                                 ) {
@@ -38,5 +38,34 @@ public class TreasureController {
                     treasure.setCharacter(character);
                     return treasureRepository.save(treasure);
                 }).orElseThrow(() -> new ResourceNotFoundException("Campaign not found with id " + characterId));
+    }
+    
+    @PutMapping("/api/v1/characters/{characterId}/treasures/{treasureId}")
+    public Treasure updateTreasure(@PathVariable Long characterId,
+                                   @PathVariable Long treasureId,
+                                   @Valid @RequestBody Treasure treasureRequest
+                                   ) {
+        if(!characterRepository.existsById(characterId)) {
+            throw new ResourceNotFoundException("Character not found with id " + characterId);
+        }
+        return treasureRepository.findById(treasureId)
+                .map(treasure -> {
+                    treasure.setTitle(treasureRequest.getTitle());
+                    return treasureRepository.save(treasure);
+                }).orElseThrow(() -> new ResourceNotFoundException("Treasure not found with id " + treasureId));
+    }
+    
+    @DeleteMapping("/api/v1/characters/{characterId}/treasures/{treasureId}")
+    public ResponseEntity<?> deleteEncounter(@PathVariable Long characterId,
+                                             @PathVariable Long treasureId
+                                            ) {
+        if(!characterRepository.existsById(characterId)) {
+            throw new ResourceNotFoundException("Character not found with id " + characterId);
+        }
+        return treasureRepository.findById(treasureId)
+                .map(treasure -> {
+                    treasureRepository.delete(treasure);
+                    return ResponseEntity.ok().build();
+                }).orElseThrow(() -> new ResourceNotFoundException("Treasure not found with id " + treasureId));
     }
 }
